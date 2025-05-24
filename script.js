73e86d0b2b9589a3ff7b921ff056ced37845f269
Sriyan-Nakka <sriyannakka@gmail.com>
2025-05-24 10:28:33 +0530

    version 0.3

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const passwordDisplay = document.querySelector("#passwordDisplay");
+const generateButton = document.querySelector("#generateButton");
+const passwordDiv = document.querySelector("#passwordDiv");
+
+const includeUppercase = document.querySelector("#includeUppercase");
+const includeNumbers = document.querySelector("#includeNumbers");
+const includeSpecialChars = document.querySelector("#specialChars");
+
 function generatePassword(e) {
   e.preventDefault();
+  let characterNum = Number(document.querySelector("#characterNum").value);
+  console.log(characterNum);
+  if (includeUppercase.checked) {
+    console.log("Include uppercase");
+  }
+  if (includeNumbers.checked) {
+    console.log("Include numbers");
+  }
+  if (includeSpecialChars.checked) {
+    console.log("Include special characters");
+  }
 }
